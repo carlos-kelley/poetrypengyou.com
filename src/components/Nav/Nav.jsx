cc789f6ca67bcc860fd79e607b24f1fd4309d4dc
Carlos Kelley <carloskelley@protonmail.com>
2023-02-17 22:54:36 -0600

    cleanup nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
 
   return (
     <div className="nav">
-      <Link to="/home">
+      <Link to="/poemselect">
         <h2 className="nav-title">
           Poetry Pengyou
         </h2>
@@ -26,9 +26,9 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
+            {/* <Link className="navLink" to="/user">
               Home
-            </Link>
+            </Link> */}
             <Link className="navLink" to="/poemselect">
               Poems
             </Link>
