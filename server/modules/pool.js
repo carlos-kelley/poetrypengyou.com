9524e3a7c42ae35144863663858fb91da485e380
Carlos Kelley <carloskelley@protonmail.com>
2023-02-21 19:29:42 -0600

    trying connectionstring

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,5 +1,7 @@
 const pg = require("pg");
 let pool;
+const connectionString =
+  "postgresql://dbmasteruser:5E9z6O&!,?SQ~G%,2B*w|K*_@ls-835aa6aa5267dacd4bcbe9d529786ed88ce408e8.chtdfiy6xaz0.us-east-2.rds.amazonaws.com:5432/poetrypengyou";
 
 // When our app is deployed to the internet
 // we'll use the DATABASE_URL environment variable
@@ -7,11 +9,12 @@ let pool;
 // eg:
 //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
 pool = new pg.Pool({
-  user: "dbmasteruser",
-  host: "ls-835aa6aa5267dacd4bcbe9d529786ed88ce408e8.chtdfiy6xaz0.us-east-2.rds.amazonaws.com",
-  database: "poetrypengyou",
-  password: "5E9z6O&!,?SQ~G%,2B*w|K$PQxC*_",
-  port: 5432,
+  connectionString,
+});
+
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  pool.end();
 });
 
 // When we're running this app on our own computer
