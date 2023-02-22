e64034522447612266ef7ba2c82098b65e5abd2f
Carlos Kelley <carloskelley@protonmail.com>
2023-02-21 19:10:00 -0600

    changed hardcoded pass, but need to make an env

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -6,24 +6,23 @@ let pool;
 // to set the connection info: web address, username/password, db name
 // eg:
 //  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
-  pool = new pg.Pool({
-    user: "dbmasteruser",
-    host: "ls-835aa6aa5267dacd4bcbe9d529786ed88ce408e8.chtdfiy6xaz0.us-east-2.rds.amazonaws.com",
-    database: "poetrypengyou",
-    password: "5E9z6O&!,?SQ~[;G%,2;B*w|K$PQxC*_",
-    port: 5432,
-  });
+pool = new pg.Pool({
+  user: "dbmasteruser",
+  host: "ls-835aa6aa5267dacd4bcbe9d529786ed88ce408e8.chtdfiy6xaz0.us-east-2.rds.amazonaws.com",
+  database: "poetrypengyou",
+  password: "5E9z6O&!,?SQ~G%,2B*w|K$PQxC*_",
+  port: 5432,
+});
 
 // When we're running this app on our own computer
 // we'll connect to the postgres database that is
 // also running on our computer (localhost)
 
-  // pool = new pg.Pool({
-  //   host: "localhost",
-  //   port: 5432,
-  //   database: "poetry",
-  //   idleTimeoutMillis: 30000,
-  // });
-
+// pool = new pg.Pool({
+//   host: "localhost",
+//   port: 5432,
+//   database: "poetry",
+//   idleTimeoutMillis: 30000,
+// });
 
 module.exports = pool;
