1ce51d9392c8afc5fc39f552103ecd59280dfaa1
Carlos Kelley <carloskelley@protonmail.com>
2023-03-03 13:02:17 -0600

    removed unused reducers

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,8 +3,6 @@ import errors from "./errors.reducer";
 import user from "./user.reducer";
 import poem from "./poem.reducer";
 import word from "./word.reducer";
-import nextPoem from "./nextPoem.reducer";
-import lastPoem from "./lastPoem.reducer";
 import allPoems from "./allPoems.reducer";
 
 // rootReducer is the primary reducer for our entire project
@@ -18,9 +16,6 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   poem, // will have poem data
   word, // will have word data
-  // character, // will have character data
-  nextPoem, // will have next poem data
-  lastPoem, // will have last poem data
   allPoems, // will have all poems data
 });
 
